3560bb9887e890de4d5cd7e41447de5d16174fad
afiqbusari7 <2000476@sit.singaporetech.edu.sg>
2021-11-15 21:14:14 +0800

    updated web portal

--- webPortal.py
+++ webPortal.py
@@ -14,7 +14,7 @@ app.config['SECRET_KEY'] = 'secret123'
 # Config MySQL
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = 'Password123!'
+app.config['MYSQL_PASSWORD'] = ''
 app.config['MYSQL_DB'] = 'mydb'
 app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
 # init MYSQL
@@ -277,7 +277,7 @@ def student_dashboard():
     cur = mysql.connection.cursor()
 
     # Show articles only from the user logged in
-    result = cur.execute("SELECT * FROM Student where email='rainyy@gmail.com'")
+    result = cur.execute("SELECT * FROM Student where email== %s", [session['username']])
 
     studentDetails = cur.fetchall()
 
