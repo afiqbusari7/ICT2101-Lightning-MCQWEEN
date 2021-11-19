ee93ff36fdb02ce5b0659d21dd2d9e762204a08e
afiqbusari7 <2000476@sit.singaporetech.edu.sg>
2021-11-19 14:06:17 +0800

    Updated DB
    Please do not hard code the emaiil

--- webPortal.py
+++ webPortal.py
@@ -14,7 +14,7 @@ app.config['SECRET_KEY'] = 'secret123'
 # Config MySQL
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = 'Password123!'
+app.config['MYSQL_PASSWORD'] = ''
 app.config['MYSQL_DB'] = 'mydb'
 app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
 # init MYSQL
@@ -277,7 +277,8 @@ def student_dashboard():
     cur = mysql.connection.cursor()
 
     # Show articles only from the user logged in
-    result = cur.execute("SELECT * FROM Student where email='rainyy@gmail.com'")
+    email_value = cur.execute("SELECT email FROM student WHERE email=%s", [session['email']])
+    result = cur.execute("SELECT * FROM Student where email=%s", [session['email']])
 
     studentDetails = cur.fetchall()
 
